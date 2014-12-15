33151fa70c7322c24c62f00c020600a8657c1e6f
kwang90 <kun.wang@live.cn>
2014-12-15 23:03:31 +0100

    webview

---
+++ Eclipse Working Place/WEB_Access/src/com/example/web_access/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.web_access;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		
+		 WebView webview = (WebView)findViewById(R.id.myWebView);
+		 webview.getSettings().setJavaScriptEnabled(true);
+		 final Activity activity = this;
+		 webview.setWebViewClient(new WebViewClient() {
+		   public void onProgressChanged(WebView view, int progress) {
+		     // Activities and WebViews measure progress with different scales.
+		     // The progress meter will automatically disappear when we reach 100%
+		     activity.setProgress(progress * 1000);
+		   }
+		 });
+		 webview.setWebViewClient(new WebViewClient() {
+		   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+		     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+		   }
+		 });
+		 webview.loadUrl("https://www.google.de/");
+		
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
